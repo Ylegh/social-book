6f6087388ad4ff559217d9be889cf35783aa2fe8
ylegh <yleghnider@deloitte.co.uk>
2023-02-27 17:20:46 +0000

    Adding like functionality, updating post feed and profile pages

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -1,17 +1,20 @@
+from hashlib import new
 import re
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .models import Profile, Post
+from .models import LikePost, Profile, Post
 
 # Create your views here.
 @login_required(login_url='signin')
 def index(request):
     user_object = User.objects.get(username=request.user.username)
     user_profile = Profile.objects.get(user=user_object)
-    return render(request, 'index.html', {'user_profile': user_profile})
+    
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'user_profile': user_profile, 'posts': posts})
 
 @login_required(login_url='signin')
 def upload(request):
@@ -19,7 +22,7 @@ def upload(request):
         user = request.user.username
         image = request.FILES.get('image_upload')
         caption = request.POST['caption']
-        
+
         new_post = Post.objects.create(user=user, image=image, caption=caption)
         new_post.save()
 
@@ -27,6 +30,42 @@ def upload(request):
     else: 
         return redirect('/')
     return HttpResponse('<h1>Upload View</h1>')
+
+@login_required(login_url='signin')
+def like_post(request):
+    username = request.user.username
+    post_id = request.GET.get('post_id')
+
+    post = Post.objects.get(id=post_id)
+
+    like_filter = LikePost.objects.filter(post_id=post_id, username=username).first()
+
+    if like_filter == None:
+        new_like = LikePost.objects.create(post_id=post_id, username=username)
+        new_like.save()
+        post.no_of_likes = post.no_of_likes+1
+        post.save()
+        return redirect('/')
+    else:
+        like_filter.delete()
+        post.no_of_likes = post.no_of_likes-1
+        post.save()
+        return redirect('/')
+
+def profile(request, pk):
+    user_object = User.objects.get(username=pk)
+    user_profile = Profile.objects.get(user=user_object)
+    user_posts = Post.objects.filter(user=pk)
+    user_post_length = len(user_posts)
+    
+    context = {
+        'user_obect': user_object,
+        'user_profile': user_profile,
+        'user_posts': user_posts,
+        'user_post_length': user_post_length,
+    }
+
+    return render(request, 'profile.html', context)
 
 @login_required(login_url='signin')
 def settings(request):
