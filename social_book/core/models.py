098b7c9fa4d2db1d9cbf78d21e010975c7d9d385
ylegh <yleghnider@deloitte.co.uk>
2023-02-28 10:52:16 +0000

    Adding follow/unfollow functionality, user search and user suggestions, fixing post feed

--- social_book/core/models.py
+++ social_book/core/models.py
@@ -1,23 +1,17 @@
-from distutils.command.upload import upload
-from operator import mod
-from statistics import mode
 from django.db import models
 from django.contrib.auth import get_user_model
-from matplotlib import image
-from numpy import true_divide
-from pyparsing import conditionAsParseAction
-import uuid 
+import uuid
 from datetime import datetime
 
 User = get_user_model()
+
 # Create your models here.
-class Profile(models.Model): 
+class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     id_user = models.IntegerField()
     bio = models.TextField(blank=True)
     profileimg = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
     location = models.CharField(max_length=100, blank=True)
-
 
     def __str__(self):
         return self.user.username
@@ -39,3 +33,10 @@ class LikePost(models.Model):
 
     def __str__(self):
         return self.username
+
+class FollowersCount(models.Model):
+    follower = models.CharField(max_length=100)
+    user = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.user
